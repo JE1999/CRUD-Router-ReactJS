a26b0659e2d51792e604e28d697d04c9ad51a7c2
Jose Alvarez <jose.alvarez@Falconeye.local>
2020-01-08 16:00:14 -0400

    Header and footer

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment } from 'react';
+
+//AXIOS PARA CONSULTAR LA API || REACT ROUTER DOM PARA LAS URLS
+//npm install --save axios react-router-dom
+
+//Layouts
+import Header from './Components/Layouts/Header'
+import Footer from './Components/Layouts/Footer'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Fragment>
+      <Header/>
+
+      <Footer/>
+    </Fragment>
+  )
 }
 
-export default App;
+export default App
---
+++ src/Components/Layouts/Footer.jsx
@@ -0,0 +1,15 @@
+import React, { Fragment } from 'react'
+
+function Footer(){
+
+    return(
+        <Fragment>
+            <footer className="page-footer border font-small blue">
+                <div className="footer-copyright text-center py-3">© 2020 Copyright: JE</div>
+            </footer>
+        </Fragment>
+    )
+
+}
+
+export default Footer
